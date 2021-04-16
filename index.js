c2eb561c51248bcbd0bb71012b59306043f202e7
Noel-96 <noelorion69@gmail.com>
2021-04-16 17:10:33 +0100

    more fixes

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port =  process.env.PORT || 5001
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
 })
+
+//,"server": "nodemon index.js",
+//"test": "cross-env NODE_ENV=testing jest --watch --verbose --runInBand --silent",
+//"resetdb": "knex migrate:rollback && knex migrate:latest && knex seed:run"
